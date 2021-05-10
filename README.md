2ace2f26de3e4e127417a41aad2e8e9b71a6b8a5
Rahma Sabry <rahmasabry94@gmail.com>
2021-05-10 22:08:27 +0200

    edit readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Laravel Fill Record Package
 This package provides a trait that will fill traced columns when saving any or updating Eloquent model.
+
+# Installation
+```php
+composer require rahmasa/fill-record
+```
+# usage
+use trait in every model has this columns
+['created_by_user','updated_by_user','created_by_ip','updated_by_ip]
+
+```php
+use Rahmasa\FillRecord\FillRecord;
+
+class Branch extends Model
+{
+    use FillRecord;
+```
--- src/FillRecord.php
+++ src/FillRecord.php
@@ -1,6 +1,6 @@
 <?php
 namespace Rahmasa\FillRecord;
-
+use Request;
 
 trait FillRecord
 {
@@ -8,12 +8,12 @@ trait FillRecord
 	{
 		static::creating(function ($model) {
 			$model->created_by_user = auth()->user()->id;
-			$model->created_by_ip = \Request::ip();
+			$model->created_by_ip = Request::ip();
 		});
 		
 		static::updating(function ($model) {
 			$model->updated_by_user = auth()->user()->id;
-			$model->updated_by_ip = \Request::ip();
+			$model->updated_by_ip = Request::ip();
 		});
 	}
 
